88c5a96efa41b0ca7ddf1075a295051a69674a07
tomygiordev <tomasroldangiorgi@gmail.com>
2025-08-19 00:49:30 -0300

    WP

--- WP SYSTEM/PlayerUI/Form2.designer.cs
+++ WP SYSTEM/PlayerUI/Form2.designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button5 = new System.Windows.Forms.Button();
+            this.panelGestionMateriales = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.panelMaterialesLista = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.SuspendLayout();
             // 
             // button5
@@ -46,12 +48,35 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // panelGestionMateriales
+            // 
+            this.panelGestionMateriales.BackColor = System.Drawing.Color.Transparent;
+            this.panelGestionMateriales.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelGestionMateriales.FillColor = System.Drawing.Color.White;
+            this.panelGestionMateriales.Location = new System.Drawing.Point(0, 0);
+            this.panelGestionMateriales.Name = "panelGestionMateriales";
+            this.panelGestionMateriales.ShadowColor = System.Drawing.Color.Black;
+            this.panelGestionMateriales.Size = new System.Drawing.Size(277, 556);
+            this.panelGestionMateriales.TabIndex = 8;
+            // 
+            // panelMaterialesLista
+            // 
+            this.panelMaterialesLista.BackColor = System.Drawing.Color.Transparent;
+            this.panelMaterialesLista.FillColor = System.Drawing.Color.White;
+            this.panelMaterialesLista.Location = new System.Drawing.Point(304, 48);
+            this.panelMaterialesLista.Name = "panelMaterialesLista";
+            this.panelMaterialesLista.ShadowColor = System.Drawing.Color.Black;
+            this.panelMaterialesLista.Size = new System.Drawing.Size(565, 496);
+            this.panelMaterialesLista.TabIndex = 9;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(38)))), ((int)(((byte)(35)))));
             this.ClientSize = new System.Drawing.Size(881, 556);
+            this.Controls.Add(this.panelMaterialesLista);
+            this.Controls.Add(this.panelGestionMateriales);
             this.Controls.Add(this.button5);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -61,5 +86,7 @@
 
         #endregion
         private System.Windows.Forms.Button button5;
+        private Guna.UI2.WinForms.Guna2ShadowPanel panelGestionMateriales;
+        private Guna.UI2.WinForms.Guna2ShadowPanel panelMaterialesLista;
     }
 }
